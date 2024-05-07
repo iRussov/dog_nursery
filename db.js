ebcde3af44792c1e2b3539ae067c747f36a10f44
AlenaDance <russov2016@gmail.com>
2024-05-08 00:37:49 +0300

    db

--- db.js
+++ db.js
@@ -1,12 +1,15 @@
 import pgPromise from 'pg-promise';
+import * as dotenv from 'dotenv';
+dotenv.config();
+
 const pgp = pgPromise();
 
 const db = pgp({
-    host: 'localhost',
-    port: 5432,
-    database: 'Dogs',
-    user: 'postgres',
-    password: '0451',
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  database: process.env.DB_NAME,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
   });
 
 
